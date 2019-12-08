28aeed4e9d17ff66e75fa23a3f6d18e72c9b0e5a
Vedant Raval <32142369+Vedant2311@users.noreply.github.com>
2019-12-08 13:59:15 +0800

    Update doubleSS.h

--- doubleSS.h
+++ doubleSS.h
@@ -25,7 +25,12 @@ class doubleSS
 
 	public:
 
-		doubleSS(int M2, double epsilon, double phi, double delta, int m): M2(M2), phi(phi), epsilon(epsilon), delta(delta), m(m) {T1= new spacesaving((int)floor(2.0/phi),epsilon,phi,m); T2= new spacesaving(M2,epsilon,phi,m); bobhash=new BOBHash64(1003);}
+		doubleSS(int M2, double epsilon, double phi, double delta, int m): M2(M2), phi(phi), epsilon(epsilon), delta(delta), m(m) {
+
+			cout << "The size of T1 is" << floor(2.0/phi) << endl;
+			cout << "The size of T2 is" << M2 << endl;
+
+			T1= new spacesaving((int)floor(2.0/phi),epsilon,phi,m); T2= new spacesaving(M2,epsilon,phi,m); bobhash=new BOBHash64(floor(2 * M2 * M2 / delta));}
 
 		unsigned long long Hash(string ST)
         {
@@ -36,6 +41,11 @@ class doubleSS
 		{
 
 			T1 ->Insert(x);
+//			cout << "Original String is " << x << endl;
+//			cout << "Hashed value is " << Hash(x) << endl;
+//			cout << "Hashed String is " << to_string(Hash(x)) << endl;
+
+//			cout << endl;
 			T2 ->Insert(to_string(Hash(x)));
 		}
 
@@ -44,13 +54,14 @@ class doubleSS
         
 		void work()
 		{
-
 			int CNT=0;
             for(int i=N;i;i=T1->ss->Left[i]){
                 for(int j=T1->ss->head[i];j;j=T1->ss->Next[j]) {
-                	if((T1->ss->sum[j] >= (floor(phi*M/2.0)))){
 
-                		if(T2->ss->sum[j] >= (floor((phi - epsilon)*M))){
+                	if((T1->ss->sum[j] >= (floor(phi*m/2.0)))){
+				//		cout << "Combo " << j << " " << T2->ss->sum[j] << endl;              
+				//		cout << "Comparision value " << (floor((phi - epsilon)*m)) << endl;
+                		if(T2->ss->sum[j] >= (floor((phi - epsilon)*m))){
                 			q[CNT].x=T1->ss->str[j]; q[CNT].y=T2->ss->sum[j]; CNT++;
                 		}
 
