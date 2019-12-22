ab86bff5937aae99cbba8f8aeba159844275288d
Vedant Raval <32142369+Vedant2311@users.noreply.github.com>
2019-12-22 22:07:23 +0800

    Update heavykeeper.h

--- Top K/heavykeeper.h
+++ Top K/heavykeeper.h
@@ -1,6 +1,3 @@
-#ifndef _heavykeeper_H
-#define _heavykeeper_H
-
 #include <cmath>
 #include <cstdio>
 #include <cstdlib>
@@ -11,101 +8,98 @@
 #include "BOBHASH32.h"
 #include "params.h"
 #include "ssummary.h"
-#include "BOBHASH64.h"
-#define HK_d 2
-#define HK_b 1.08
-#define rep(i,a,n) for(int i=a;i<=n;i++)
-using namespace std;
-class heavykeeper
-{
-    private:
-        ssummary *ss;
-        struct node {int C,FP;} HK[HK_d][MAX_MEM+10];
-        BOBHash64 * bobhash;
-        int M2,K;
+#include "BOBHASH32.h"
+#include "spacesaving.h"
 
-    public:
-        heavykeeper(int M2,int K):K(K),M2(M2) {ss=new ssummary(K); ss->clear(); bobhash=new BOBHash64(1005);}
-        void clear()
-        {
-            for (int i=0; i<HK_d; i++)
-                for (int j=0; j<=M2+5; j++) HK[i][j].C=HK[i][j].FP=0;
-        }
-        unsigned long long Hash(string ST)
+double A = 0.61803398875;
+unsigned int maxVal;
+
+using namespace std;
+
+class doubleSS
+{
+
+	private:
+		spacesaving *T1;
+		spacesaving *T2;
+		BOBHash32 *bobhash;
+		int M2,K;
+		double delta;
+
+	public:
+
+		doubleSS(int M2, int K, double delta): M2(M2), delta(delta), K(K) {
+
+			maxVal = (floor((2.0 * M2 * M2 / delta)));			
+			T1= new spacesaving((M2/30) * K); T2= new spacesaving(M2); bobhash=new BOBHash32(1003);
+
+		}
+
+		unsigned int Hash(string ST)
         {
             return (bobhash->run(ST.c_str(),ST.size()));
         }
-        void Insert(string x)
-        {
-            bool mon=false;
-            int p=ss->find(x);
-            if (p) mon=true;
-            int maxv=0;
-            unsigned long long H=Hash(x); int FP=(H>>48);
-            for (int j=0; j<HK_d; j++)
-            {
-                int Hsh=H%(M2-(2*HK_d)+2*j+3);
-                int c=HK[j][Hsh].C;
-                if (HK[j][Hsh].FP==FP)
-                {
-                    if (mon || c<=ss->getmin())
-                      HK[j][Hsh].C++;
-                    maxv=max(maxv,HK[j][Hsh].C);
-                } else
-                {
-                    if (!(rand()%int(pow(HK_b,HK[j][Hsh].C))))
-                    {
-                        HK[j][Hsh].C--;
-                        if (HK[j][Hsh].C<=0)
-                        {
-                            HK[j][Hsh].FP=FP;
-                            HK[j][Hsh].C=1;
-                            maxv=max(maxv,1);
-                        }
-                    }
-                }
-            }
-            if (!mon)
-            {
-                if (maxv-(ss->getmin())==1 || ss->tot<K)
-                {
-                    int i=ss->getid();
-                    ss->add2(ss->location(x),i);
-                    ss->str[i]=x;
-                    ss->sum[i]=maxv;
-                    ss->link(i,0);
-                    while(ss->tot>K)
-                    {
-                        int t=ss->Right[0];
-                        int tmp=ss->head[t];
-                        ss->cut(ss->head[t]);
-                        ss->recycling(tmp);
-                    }
-                }
-            } else
-            if (maxv>ss->sum[p])
-            {
-                int tmp=ss->Left[ss->sum[p]];
-                ss->cut(p);
-                if(ss->head[ss->sum[p]]) tmp=ss->sum[p];
-                ss->sum[p]=maxv;
-                ss->link(p,tmp);
-            }
-        }
-        struct Node {string x; int y;} q[MAX_MEM+10];
+
+		void Insert(string x)
+		{
+
+			T1 ->Insert(x);
+
+			unsigned int out = Hash(x);
+			unsigned int final = floor(maxVal * ((A*out) - (floor(A*out))));
+
+			T2 ->Insert(to_string(final));
+		}
+
+		struct Node {string x; int y;} q1[MAX_MEM+10];
+		Node q[MAX_MEM+10];
         static int cmp(Node i,Node j) {return i.y>j.y;}
-        int work()
-        {
+        
+        int isTopK(Node* q1, string str){
+
+        	for(int i=0; i<K; i++){
+        		if (q1[i].x==str) return i;
+        	}
+
+        	return -1;
+
+        }
+
+		int work()
+		{
+            int CNT1=0;
+            for(int i=N;i;i=T2->ss->Left[i])
+                for(int j=T2->ss->head[i];j;j=T2->ss->Next[j]) {q1[CNT1].x=T2->ss->str[j]; q1[CNT1].y=T2->ss->sum[j]; CNT1++; }
+
+            sort(q1,q1+CNT1,cmp);
+
+        	for(int i=0; i<K;i++)	cout <<"Top " << i << " is " << q1[i].y << endl;
+
             int CNT=0;
-            for(int i=N;i;i=ss->Left[i])
-                for(int j=ss->head[i];j;j=ss->Next[j]) {q[CNT].x=ss->str[j]; q[CNT].y=ss->sum[j]; CNT++; }
+            for(int i=N;i;i=T1->ss->Left[i])
+                for(int j=T1->ss->head[i];j;j=T1->ss->Next[j]){
+
+                	unsigned int v = Hash(T1->ss->str[j]);
+					unsigned int final = floor(maxVal * ((A*v) - (floor(A*v))));
+
+					int ind = isTopK(q1,to_string(final));
+
+					if (ind==-1) continue;
+
+					q[CNT].x=T1->ss->str[j]; 
+					q[CNT].y=q1[ind].y; 
+					CNT++; 					
+
+                }
+
             sort(q,q+CNT,cmp);
             return CNT;
 
-        }
-        pair<string ,int> Query(int k)
+		}
+
+	pair<string ,int> Query(int k)
         {
             return make_pair(q[k].x,q[k].y);
         }
+
 };
-#endif
